0808f0dc64a623656663da647a42fa254a5e1fbb
RamadaniEdon <92359718+RamadaniEdon@users.noreply.github.com>
2024-01-24 01:45:38 +0200

    set up database connection using shark edon

--- src/city/org/rs/ConnectionUtility.java
+++ src/city/org/rs/ConnectionUtility.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionUtility {
 
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/mydb";
-    private static final String DATABASE_USER = "admin";
-    private static final String DATABASE_PASSWORD = "mysql";
+    private static final String DATABASE_URL = "jdbc:mysql://ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com/pesyjzyyer5847fj";
+    private static final String DATABASE_USER = "m3gsybf3a83j0kg8";
+    private static final String DATABASE_PASSWORD = "bjg8lure1rmtso67";
 
     // private static final String DATABASE_URL = "jdbc:mysql://r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com/kqkeb9e5irew8xi1";
     // private static final String DATABASE_USER = "vxsnasuvirkou9jo";
