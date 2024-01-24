40e84aafdcf86eedeaeab8f806fd84477a260aa1
RamadaniEdon <92359718+RamadaniEdon@users.noreply.github.com>
2024-01-24 02:42:28 +0200

    Update CORSFilter.java

--- src/city/org/rs/utils/CORSFilter.java
+++ src/city/org/rs/utils/CORSFilter.java
@@ -19,10 +19,10 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        // response.setHeader("Access-Control-Allow-Origin", "*");
-        // response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        // response.setHeader("Access-Control-Max-Age", "3600");
-        // response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, endDate, startDate");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, endDate, startDate");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
