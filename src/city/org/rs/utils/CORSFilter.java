3c353b347c1f4741c0a97b224420b8fc1a462341
RamadaniEdon <92359718+RamadaniEdon@users.noreply.github.com>
2024-01-24 02:36:44 +0200

    Update CORSFilter.java

--- src/city/org/rs/utils/CORSFilter.java
+++ src/city/org/rs/utils/CORSFilter.java
@@ -16,19 +16,19 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) req;
+        // HttpServletResponse response = (HttpServletResponse) res;
+        // HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, endDate, startDate");
+        // response.setHeader("Access-Control-Allow-Origin", "*");
+        // response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        // response.setHeader("Access-Control-Max-Age", "3600");
+        // response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, endDate, startDate");
 
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(req, res);
-        }
+        // if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+        //     response.setStatus(HttpServletResponse.SC_OK);
+        // } else {
+        //     chain.doFilter(req, res);
+        // }
     }
 
 }
